3f15f5b27c233fbbe602090a967104b28ce49ee5
CyberChagas <125417795+CyberChagas@users.noreply.github.com>
2023-04-05 21:33:13 -0300

    Add files via upload

---
+++ Atividade6/MediaJS.js
@@ -0,0 +1,18 @@
+
+
+var nome; //Inovação nos nomes de variaveis é comigo mesmo
+var n1, n2, n3; 
+var media; // criativo como sempre
+
+nome = prompt("Qual é o seu nome?");
+n1 = prompt("Digite a primeira nota: ");
+n2 = prompt("Digite a segunda nota: ");
+n3 = prompt("Digite a terceira nota: ");
+
+media = (parseFloat(n1) + parseFloat(n2) + parseFloat(n3)) / 3;
+
+alert("A média do aluno chamado " + nome + " é: " + media);
+
+
+
+
---
+++ Atividade6/OperacoesJS.js
@@ -0,0 +1,17 @@
+
+
+var nome; //Inovação nos nomes de variaveis é comigo mesmo
+var n1, n2;
+var media, soma, sub, produto, division, restinho; // Perdoa os nomes, por favor
+
+n1 = prompt("Digite o primeiro número: ");
+n2 = prompt("Digite o segundo número: ");
+
+soma = (parseFloat(n1) + parseFloat(n2));
+sub = (parseFloat(n1) - parseFloat(n2));
+produto = (parseFloat(n1) * parseFloat(n2));
+division = (parseFloat(n1) / parseFloat(n2));
+restinho= (parseFloat(n1) % parseFloat(n2));
+
+
+alert("Soma: " + soma + " \nSubtração: " + sub + "\nProduto: " + produto + "\nDivisão: " + division + "\nResto da divisão: " + restinho);
