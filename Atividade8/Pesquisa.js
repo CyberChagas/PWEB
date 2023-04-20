832f6b5b7fbf4a116c9c0a948d48c0b4285b9ba6
Gabriel Chagas <125417795+CyberChagas@users.noreply.github.com>
2023-04-19 21:39:07 -0300

    Add files via upload

---
+++ Atividade8/Pesquisa.js
@@ -0,0 +1,27 @@
+﻿
+
+/*
+
+
+Vou corrigir o programa no feriado, não sei pq estou tendo bastante problemas na hora de 'resetar' o formulario, salvando os dados e exibindo os valores.
+
+Tentei limpar, mas acho que no feriado corrigo tudo.
+
+(idade,sexoeopinião) e retornar: a média da idade das pessoasque responderam ao questionário;a idade da pessoa mais
+velha;a idade da pessoa mais nova;a quantidadede pessoas que responderam péssimo;a porcentagemde pessoas que
+responderam ótimo e bom;o número de mulherese homensque responderam ao questionário.
+
+    idades.push(idade);
+    document.getElementById('idade').value = ""; // Limpar o campo de idade
+
+*/
+
+
+function salvarIdade(idade) {
+    alert("Sua idade é: " idade);
+  }
+
+  
+function exibirIdades() {
+  alert("Idades: " + idades.join(", "));
+}
