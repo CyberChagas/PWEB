4b521652f049091dc9380c9db9dd12bf1387da4c
Gabriel Chagas <125417795+CyberChagas@users.noreply.github.com>
2023-04-25 00:04:13 -0300

    Add files via upload

---
+++ Atividade9/duasfuncoes.js
@@ -0,0 +1,22 @@
+﻿function maiorNumero(num1, num2, num3) {
+  let maior = num1;
+
+  if (num2 > maior) {
+    maior = num2;
+  }
+
+  if (num3 > maior) {
+    maior = num3;
+  }
+
+  return maior;
+}
+
+function ordenarNumeros(num1, num2, num3) {
+  let numeros = [num1, num2, num3];
+  numeros.sort(function(a, b) {
+    return a - b;
+  });
+
+  return numeros;
+}
